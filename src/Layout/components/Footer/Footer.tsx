861bb55e546b4cc1a6eac2a48f67d2105bbb3657
tktrbva <tktrbva2401@gmal.com>
2024-03-01 23:39:50 +0600

    feat/footer

--- src/Layout/components/Footer/Footer.tsx
+++ src/Layout/components/Footer/Footer.tsx
@@ -1,8 +1,52 @@
-// import style from "./style.module.scss";
+import style from "./style.module.scss";
+import logo from "../img/fontisto_tripadvisor.png";
+import whatsApp from "../img/whatsapp.png";
+import facebook from "../img/facebook.png";
+import insta from "../img/instagram.png";
 
 const Footer = () => {
-  return <></>;
+  return (
+    <>
+      <footer className={style.header}>
+        <article className={style.header_left}>
+          <h2>Kyrgyz Traces</h2>
+          <p>
+            We are proudly the Best Inside Travel DMCs & Tour Operators in each
+            destination where you travel to, our. Operat... <b>Read more</b>
+          </p>
+        </article>
+        <article className={style.header_middle}>
+          <h2>CONTACT US</h2>
+          <p>
+            <b>Email: </b>
+            experts@kyrgyzriders.kg
+          </p>
+          <p>
+            <b>Hotline: </b>
+            +996 (555) 55-55-55
+          </p>
+          <p>
+            <b>Contact form: </b>
+            Send us a message{" "}
+          </p>
+        </article>
+        <article className={style.header_right}>
+          <h2>Follow us</h2>
+          <section className={style.header_right__logos}>
+            <img src={logo} alt="" />
+            <img src={whatsApp} alt="" />
+            <img src={facebook} alt="" />
+            <img src={insta} alt="" />
+          </section>
+        </article>
+      </footer>
+      <hr />
+      <footer className={style.footer}>
+        <p>@ 2023 Kyrgyz Riders. All rights reserved</p>
+        <p>Design by @SolidDevs</p>
+      </footer>
+    </>
+  );
 };
 
-
-export default Footer
+export default Footer;
