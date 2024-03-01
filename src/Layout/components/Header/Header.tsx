966801bac95feba0c2fef8304a852f9ee8d82add
tktrbva <tktrbva2401@gmal.com>
2024-03-01 19:16:02 +0600

    fix/header

--- src/Layout/components/Header/Header.tsx
+++ src/Layout/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import style from "./style.module.scss";
 import search from "../img/searchIcon.svg";
+import { useMemo } from "react";
 
 const Header = () => {
   interface Link {
@@ -15,21 +16,23 @@ const Header = () => {
     { link: "Share tours", href: "" },
   ];
 
+  const memoLinks = useMemo(() => {
+    return links.map((a, id) => {
+      return (
+        <a key={id} href={a.href}>
+          {a.link}
+        </a>
+      );
+    });
+  }, [links]);
+
   return (
     <>
       <header className={style.header}>
         <div className={style.header_left}>
           <h3>Kyrgyz Traces</h3>
           <hr />
-          <section className={style.header_links}>
-            {links.map((a, id) => {
-              return (
-                <a key={id} href={a.href}>
-                  {a.link}
-                </a>
-              );
-            })}
-          </section>
+          <section className={style.header__links}>{memoLinks}</section>
         </div>
         <div className={style.header_right}>
           <input placeholder="Search tour" type="text" />
