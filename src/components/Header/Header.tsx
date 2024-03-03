25bf94cb52c78d2036b334b9fb5a10f12720b967
tktrbva <tktrbva2401@gmal.com>
2024-03-03 13:35:11 +0600

    fix header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import style from "./style.module.scss";
 import search from "../img/searchIcon.svg";
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
   interface Link {
@@ -8,6 +9,9 @@ const Header = () => {
     href: string;
   }
 
+  const [select, setSelect] = useState<string>("all");
+  const location = useLocation()
+  
   const links: Link[] = [
     { link: "Home", href: "/" },
     { link: "Tours", href: "/Tours" },
@@ -15,22 +19,37 @@ const Header = () => {
     { link: "About us", href: "/AboutUs" },
     { link: "Share tours", href: "/ShareTours" },
   ];
+  
+  const clickSelect = (link: string) => {
+    setSelect(link);
+  };
+
+  useEffect(() => {
+    console.log("select changed:", select);
+  }, [select]);
 
   const memoLinks = useMemo(() => {
     return links.map((a, id) => {
       return (
-        <a key={id} href={a.href}>
+        <Link
+          onClick={() => clickSelect(a.link)}
+          className={
+            location.pathname === a.href ? style.active_select__a : style.header_links__a
+          }
+          key={id}
+          to={a.href}
+        >
           {a.link}
-        </a>
+        </Link>
       );
     });
-  }, [links]);
+  }, [links, select]);
 
   return (
     <>
       <header className={style.header}>
         <div className={style.header_left}>
-          <a href="/">Kyrgyz Traces</a>
+          <p>Kyrgyz Traces</p>
           <hr />
           <section className={style.header__links}>{memoLinks}</section>
         </div>
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,14 +1,20 @@
 import { Route, Routes } from "react-router-dom";
-import Layout from "../Layout/Layout";
+import HomePage from "../Pages/HomePage";
+import Tours from "../Pages/Tours";
+import Destinations from "../Pages/Destination";
+import AboutUs from "../Pages/AboutUs";
+import ShareTours from "../Pages/ShareTours";
 
 const AppRouter: React.FC = () => {
   return (
     <>
-      <Layout>
-        <Routes>
-          <Route path="" element/>
-        </Routes>
-      </Layout>
+     <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/Tours" element={<Tours />} />
+        <Route path="/Destinations" element={<Destinations />} />
+        <Route path="/AboutUs" element={<AboutUs />} />
+        <Route path="/ShareTours" element={<ShareTours />} />
+      </Routes>
     </>
   );
 };
