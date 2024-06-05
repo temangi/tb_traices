f987b5732d32d07ae5dc3cf07cca0e35556941db
tktrbva <tktrbva2401@gmal.com>
2024-06-06 01:44:38 +0600

    feat/button selection

--- src/components/SearchBlock/Filter/index.tsx
+++ src/components/SearchBlock/Filter/index.tsx
@@ -1,11 +1,12 @@
 import style from "./style.module.scss";
 import arrow from "../../img/tourlogo.svg";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import close from "../../img/closeArrow.svg";
 
 interface Modal {
   name: string;
   open: boolean;
+  id: number;
 }
 
 interface Card {
@@ -17,13 +18,78 @@ interface Card {
 const Filter: React.FC<Card> = ({ title, card }) => {
   const [openModal, setOpenModal] = useState(false);
   const [isModal, setIsModal] = useState(false);
+  const [select, setSelect] = useState(card);
+  const [count, setCount] = useState(0);
+  const [day, setDay] = useState("");
 
   const handleClick = () => {
     setIsModal(!isModal);
     setOpenModal(true);
   };
 
+  useEffect(() => {
+    const body = document.body;
+    body.style.overflow = isModal ? "hidden" : "";
+  }, [isModal]);
 
+  const clickBtn = (id: number) => {
+    const arr = select.map((item) => {
+      const status = item.open;
+      if (item.id === id) {
+        return { ...item, open: !status };
+      }
+      return { ...item };
+    });
+    setSelect(arr);
+  };
+
+  const clickBtnDay = (id: number) => {
+    const arr = select.map((item) => {
+      const status = item.open;
+      if (item.id === id) {
+        setDay(item.name);
+        return { ...item, open: !status };
+      }
+      return { ...item, open: false };
+    });
+    setSelect(arr);
+  };
+
+  const reset = () => {
+    const arr = select.map((item) => {
+      return { ...item, open: false };
+    });
+    setSelect(arr);
+  };
+
+  const apply = () => {
+    setIsModal(!isModal);
+  };
+
+  useEffect(() => {
+    let count: number = 0;
+
+    select.map((item) => {
+      if (item.open === true) {
+        count++;
+      }
+    });
+    setCount(count);
+  }, [count, select]);
+
+  const buttons = useMemo(() => {
+    return select.map(({ name, id, open }) => (
+      <p
+        className={open ? style.selectBtn : style.notSelectBtn}
+        onClick={
+          title === "Duration" ? () => clickBtnDay(id) : () => clickBtn(id)
+        }
+        key={id}
+      >
+        {name}
+      </p>
+    ));
+  }, [select]);
 
   const modal = useMemo(() => {
     return (
@@ -40,30 +106,40 @@ const Filter: React.FC<Card> = ({ title, card }) => {
             <img onClick={() => setOpenModal(false)} src={close} alt="" />
           </div>
           <hr />
-          <div className={style.modal__filterBtn}>
-            {card.map((item, id) => (
-              <p
-                key={id}
-              >
-                {item.name}
-              </p>
-            ))}
-          </div>
+          <div className={style.modal__filterBtn}>{buttons}</div>
           <hr />
           <article className={style.modal_btn}>
-            <button className={style.reset}>Reset</button>
-            <button className={style.apply}>Apply</button>
+            <button onClick={reset} className={style.reset}>
+              Reset
+            </button>
+            <button onClick={apply} className={style.apply}>
+              Apply
+            </button>
           </article>
         </article>
       </main>
     );
-  }, [isModal]);
+  }, [isModal, buttons]);
 
   return (
     <>
       <section onClick={handleClick} className={style.filterBtn}>
-        <p>{title}</p>
-        <img src={arrow} alt="" />
+        <div className={style.filterBtn_left}>
+          <p className={count > 0 ? style.title_active : style.title_notActive}>
+            {title}
+          </p>
+
+          <p className={style.count}>
+            {title !== "Duration" && count > 0 ? `(${count})` : ""}
+          </p>
+          <p className={style.days}>
+            {title === "Duration" && count > 0 ? `(${day})` : ""}
+          </p>
+        </div>
+        <img src={arrow} alt="arrow" />
+        <span
+          className={count > 0 ? style.circle_active : style.circle_notActive}
+        ></span>
       </section>
       {openModal && modal}
     </>
